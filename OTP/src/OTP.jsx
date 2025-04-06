4b29b295e1ba8109ebce0dff9395d0df2a75ca70
Shreyansh <shreyansh2128@gmail.com>
2025-04-06 19:50:53 +0530

    OTPfinal

--- OTP/src/OTP.jsx
+++ OTP/src/OTP.jsx
@@ -1,10 +1,55 @@
 
-import React from 'react';
+import { useEffect, useRef, useState } from "react";
 
-function otp() {
+export default function Otp({ otpLength = 6 }) {
+  const [otpFields, setOtpFields] = useState(new Array(otpLength).fill(""));
+  const ref = useRef([]);
+
+  const handleKeyDown = (e, index) => {
+    const key = e.key;
+    if (key === "ArrowLeft") {
+      if (index > 0) ref.current[index - 1].focus();
+      return;
+    }
+    if (key === "ArrowRight") {
+      if (index + 1 < otpFields.length) ref.current[index + 1].focus();
+      return;
+    }
+
+    const copyOtpFields = [...otpFields];
+    if (key === "Backspace") {
+      console.log("Delete clicked");
+      copyOtpFields[index] = "";
+      setOtpFields(copyOtpFields);
+      if (index > 0) ref.current[index - 1].focus();
+      return;
+    }
+
+    if (isNaN(key)) {
+      return;
+    }
+
+    copyOtpFields[index] = key;
+    if (index + 1 < otpFields.length) ref.current[index + 1].focus();
+    setOtpFields(copyOtpFields);
+  };
+
+  useEffect(() => {
+    ref.current["0"].focus();
+  }, []);
   return (
-    <div>otp</div>
-  )
+    <div className="container">
+      {otpFields.map((value, index) => {
+        return (
+          <input
+            key={index}
+            ref={(currentInput) => (ref.current[index] = currentInput)}
+            type="text"
+            value={value}
+            onKeyDown={(e) => handleKeyDown(e, index)}
+          />
+        );
+      })}
+    </div>
+  );
 }
-
-export default otp
